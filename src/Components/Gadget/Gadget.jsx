143ff314b1f34fec5a3e1c9b23ceb129554b522c
Avishek Banerjee <banerjeeavishek2018@gmail.com>
2024-11-10 01:01:54 +0600

    card section modify

--- src/Components/Gadget/Gadget.jsx
+++ src/Components/Gadget/Gadget.jsx
@@ -3,7 +3,7 @@
 const Gadget = ({gadget}) => {
     const {product_image, product_title, price}=gadget;
     return (
-        <div className="shadow-xl p-6 ">
+        <div className="shadow-xl p-6 rounded-2xl ">
            <img className="h-[166px] py-2 rounded-2xl mx-8" src={product_image} alt="" />
            <h1 className="text-xl font-bold">{product_title}</h1>
            <p className="font-semibold text-gray-500 py-2">Price:{price}k</p> 
--- src/Components/Gadgets/Gadgets.jsx
+++ src/Components/Gadgets/Gadgets.jsx
@@ -16,12 +16,24 @@ const Gadgets = () => {
 
     return (
         <div>
-           <h2 className="text-4xl font-bold text-center">Explore Cutting-Edge Gadgets</h2>
-           <div className="grid grid-cols-3 gap-4  border-blue-700 mx-64 " >
+           <h2 className="text-4xl font-bold text-center ">Explore Cutting-Edge Gadgets</h2>
+           <div className="flex gap-4 ">
+           <div className="grid grid-cols-1 my-80 ml-10">
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">All Product</button>
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">Laptops</button>
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">Phones</button>
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">Accessories</button>
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">Smart Watches</button>
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">MacBook</button>
+                  <button className="btn bg-white rounded-3xl border-purple-700 text-gray-600 font-bold px-20">Iphone</button>
+           </div>
+           <div className="grid grid-cols-3 gap-4  mx-64 my-8" >
             {
                 gadgets.map(gadget => <Gadget  gadget={gadget} key={gadget.product_id} ></Gadget>)
             }
            </div>
+           </div>
+          
         </div>
     );
 };
